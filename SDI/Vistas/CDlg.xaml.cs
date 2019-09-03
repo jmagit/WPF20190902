32108739a8beb968f14733ec429dde70db12b5a1
Javier <jmartin@grupoloyal.com>
2019-09-03 16:57:20 +0200

    SDI

---
+++ SDI/Vistas/CDlg.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SDI.Vistas {
+    /// <summary>
+    /// Lógica de interacción para CDlg.xaml
+    /// </summary>
+    public partial class CDlg : Window {
+        public CDlg() {
+            InitializeComponent();
+        }
+
+        private void BtnAceptar_Click(object sender, RoutedEventArgs e) {
+            this.DialogResult = true;
+        }
+
+        private void BtnCancelar_Click(object sender, RoutedEventArgs e) {
+            this.DialogResult = false;
+        }
+    }
+}
